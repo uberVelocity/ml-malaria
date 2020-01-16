6eb72de0f8884954cf46a38f3b83554c5e26b75a
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-01-16 19:39:56 +0100

    second pass random_forest.py refactor

--- random_forest.py
+++ random_forest.py
@@ -11,59 +11,50 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
 
-entries = os.environ["HOME"] + config.image_location
-parasitized_entries = os.listdir(entries+'/Parasitized')
-uninfected_entries = os.listdir(entries+'/Uninfected') 
-# global vairable
-number_bin = 4 # in image description 
-number_datasamples= 50  
-
-
-def imageDescriptor(im):
-    out_hist = [] #historgram
-    height, width =im.size
-    partial_h = int(height/number_bin) 
-    partial_w = int(width/number_bin) 
+def image_descriptor(im):
+    out_hist = []  # historgram
+    height, width = im.size
+    partial_h = int(height / number_bin)
+    partial_w = int(width / number_bin)
     im_new = im
-    new_pix=im_new.load()
-    for i in range(number_bin): 
-        for j in range(number_bin): 
-            r=b= g= counter =0 
-            for h in range(i*partial_h, (i+1)*partial_h): 
-                for w in range(j*partial_w, (j+1)*partial_w): 
-                    #object description here 
-                    ## averaging the color with the window
-                    r += new_pix[h,w][0] 
-                    g += new_pix[h,w][1]
-                    b += new_pix[h,w][2]
-                    counter+=1
-            r/=counter 
-            g/=counter 
-            b/=counter 
+    new_pix = im_new.load()
+
+    for i in range(number_bin):
+        for j in range(number_bin):
+            r = b = g = counter = 0
+            for h in range(i * partial_h, (i + 1) * partial_h):
+                for w in range(j * partial_w, (j + 1) * partial_w):
+                    # object description here
+                    # averaging the color with the window
+                    r += new_pix[h, w][0]
+                    g += new_pix[h, w][1]
+                    b += new_pix[h, w][2]
+                    counter += 1
+            r /= counter
+            g /= counter
+            b /= counter
             out_hist.append(r)
             out_hist.append(g)
             out_hist.append(b)
-    # out_hist+=list(des)
-    #print(out_hist)
     return out_hist
 
 
-def createDataFrame(): 
-    out = pd.DataFrame(columns=['label','histogram'])
-    label = 'parasitized' 
-    for index, entry in zip(range(int(number_datasamples/2)),parasitized_entries): 
-        im = Image.open(entries+'/Parasitized/'+entry)
-        hist = imageDescriptor(im)
-        out.loc[len(out)] = [label,hist] 
+def create_data_frame():
+    out = pd.DataFrame(columns=['label', 'histogram'])
+    label = 'parasitized'
+    for index, entry in zip(range(int(number_datasamples / 2)), parasitized_entries):
+        im = Image.open(entries + '/Parasitized/' + entry)
+        hist = image_descriptor(im)
+        out.loc[len(out)] = [label, hist]
 
-    label= 'Uninfected'
-    for index, entry in zip(range(int(number_datasamples/2)), uninfected_entries): 
-        im = Image.open(entries+'/Uninfected/'+entry)
-        hist = imageDescriptor(im)
-        out.loc[len(out)] = [label,hist] 
+    label = 'Uninfected'
+    for index, entry in zip(range(int(number_datasamples / 2)), uninfected_entries):
+        im = Image.open(entries + '/Uninfected/' + entry)
+        hist = image_descriptor(im)
+        out.loc[len(out)] = [label, hist]
 
     tags = out['histogram'].apply(pd.Series)
-    tags = tags.rename(columns = lambda x : 'hist_' + str(x)) 
+    tags = tags.rename(columns=lambda x: 'hist_' + str(x))
     out = pd.concat([out[:], tags[:]], axis=1)
     out = out.drop(columns=['histogram'], axis=1)
     out = out.fillna(0)
@@ -71,25 +62,37 @@ def createDataFrame():
     return out
 
 
-malaria_data = createDataFrame() 
-print(malaria_data)
+def draw_tree(rf, feature_list):
+    # Pull out one tree from the forest
+    tree = rf.estimators_[5]
+
+    export_graphviz(tree, out_file='tree.dot', feature_names=feature_list, rounded=True, precision=1)
+    (graph,) = pydot.graph_from_dot_file('tree.dot')
+    graph.write_png('tree.png')
+
+    print('The depth of this tree is:', tree.tree_.max_depth)
+
+
+entries = os.environ["HOME"] + config.image_location
+parasitized_entries = os.listdir(entries + '/Parasitized')
+uninfected_entries = os.listdir(entries + '/Uninfected')
+# global vairable
+number_bin = 4  # in image description
+number_datasamples = 50
+
+malaria_data = create_data_frame()
 
 labels = np.array(malaria_data['label'])
-features= malaria_data.drop(columns=['label'], axis = 1)
+features = malaria_data.drop(columns=['label'], axis=1)
 feature_list = list(features.columns)
 features = np.array(features)
-print(features) 
 
-train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.5,random_state = 21)
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.5,
+                                                                            random_state=21)
 
-print('Training Features Shape:', train_features.shape)
-print('Training Labels Shape:', train_labels.shape)
-print('Testing Features Shape:', test_features.shape)
-print('Testing Labels Shape:', test_labels.shape)
-
-rf = RandomForestClassifier(n_estimators= 100, random_state=42)
-rf_new = RandomForestClassifier(n_estimators = 100, criterion = 'mse', max_depth = None, 
-                               min_samples_split = 2, min_samples_leaf = 1)
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_new = RandomForestClassifier(n_estimators=100, criterion='mse', max_depth=None,
+                                min_samples_split=2, min_samples_leaf=1)
 
 rf.fit(train_features, train_labels)
 predictions = rf.predict(test_features)
@@ -102,23 +105,11 @@ mape = 100 * (errors / test_labels.shape[0])
 accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
 
-
-def draw_tree(rf, feature_list=feature_list): 
-    # Pull out one tree from the forest
-    tree = rf.estimators_[5]
-
-    export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-    (graph, ) = pydot.graph_from_dot_file('tree.dot')
-    graph.write_png('tree.png'); 
-
-    print('The depth of this tree is:', tree.tree_.max_depth)
-
-
-draw_tree(rf)
+draw_tree(rf, feature_list)
 
 print("###########Smaller tree")
 # Limit depth of tree to 2 levels
-rf_small = RandomForestClassifier(n_estimators=10,random_state=42)
+rf_small = RandomForestClassifier(n_estimators=10, random_state=42)
 rf_small.fit(train_features, train_labels)
 
 predictions = rf_small.predict(test_features)
@@ -133,8 +124,7 @@ mape = 100 * (errors / test_labels.shape[0])
 accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
 
-draw_tree(rf_small)
-
+draw_tree(rf_small, feature_list)
 
 print("###########Important features")
 # Get numerical feature importances
@@ -144,13 +134,13 @@ importances = list(rf.feature_importances_)
 feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
 
 # Sort the feature importances by most important first
-feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
 
 # Print out the feature and importances 
-[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in zip(range(5), feature_importances)];
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in zip(range(5), feature_importances)]
 
 # New random forest with only the two most important variables
-rf_most_important = RandomForestClassifier(n_estimators= 10, random_state=42)
+rf_most_important = RandomForestClassifier(n_estimators=10, random_state=42)
 
 # Extract the two most important features
 important_indices = [feature_list.index('hist_42'), feature_list.index('hist_43')]
