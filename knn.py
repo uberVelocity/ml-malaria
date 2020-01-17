cee62ab33f1738c1046bf6111358b2d599cea088
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-01-17 15:30:35 +0100

    rewrote some stuff

--- knn.py
+++ knn.py
@@ -3,13 +3,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
-def feature_scaling(X_train, X_test):
-	scaler = StandardScaler()
-	scaler.fit(X_train)
 
-	X_train = scaler.transform(X_train)
-	X_test = scaler.transform(X_test)
-	
+def feature_scaling(X_train, X_test):
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
 def load_images():
     """
     Loads processed images from the .npy file at the configured location.
@@ -43,10 +44,10 @@ def load_images():
             print(class_label)
 
     return image_arr, label_arr
-	
+
 if __name__ == '__main__':
     print("Loading images...")
-	k = 2
+    k = 2
     images, labels = load_images()
     x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.33, random_state=42)
     print(f"Images loaded! x_train: f{len(x_train)}, x_test: f{len(x_test)}")
@@ -56,11 +57,11 @@ if __name__ == '__main__':
     classifier = KNeighborsClassifier(k)
 
     print("Commencing training...")
-	classifier.fit(x_train, y_train)
+    classifier.fit(x_train, y_train)
 
-	print("Predicting...")
-	y_pred = classifier.predict(x_test)
-	
-	print("Evaluating...")
-	print(confusion_matrix(y_test, y_pred))
-	print(classification_report(y_test, y_pred))
+    print("Predicting...")
+    y_pred = classifier.predict(x_test)
+
+    print("Evaluating...")
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
