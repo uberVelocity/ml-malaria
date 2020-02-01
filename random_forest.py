0bf97b99e19772eae0dada5b4a8f5ebd3e4d23f8
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-02-01 12:01:05 +0100

    PEP-8 compliance

--- random_forest.py
+++ random_forest.py
@@ -2,17 +2,14 @@ import os
 import pydot
 import numpy as np
 import config
-import data_wrappers 
-from data_wrappers import load_image_data
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
+from data_wrappers import load_image_data
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from sklearn.feature_selection import SelectFromModel 
-from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectFromModel
 
 
 entries = os.environ["HOME"] + config.image_location
@@ -22,11 +19,10 @@ number_bin = 4  # in image description
 number_datasamples = 50
 
 
-
-def train_random_forest(train_features, test_features, train_labels, test_labels,feature_list,forest):
+def train_random_forest(train_features, test_features, train_labels, test_labels, feature_list, forest):
     forest.fit(train_features, train_labels)
     predictions = forest.predict(test_features)
-    corrects = metrics.accuracy_score(test_labels, predictions, normalize= False)
+    corrects = metrics.accuracy_score(test_labels, predictions, normalize=False)
     print('number of correct:', corrects)
     accuracy = (corrects / test_labels.shape[0])
     print('Accuracy:', accuracy)
@@ -39,67 +35,63 @@ def train_random_forest(train_features, test_features, train_labels, test_labels
 def draw_tree(rf, feature_list, name):
     # Pull out one tree from the forest
     tree = rf.estimators_[0]
-    export_graphviz(tree, out_file= name+'.dot', feature_names=feature_list, rounded=True, precision=1)
+    export_graphviz(tree, out_file=name+'.dot', feature_names=feature_list, rounded=True, precision=1)
     (graph,) = pydot.graph_from_dot_file(name+'.dot')
     graph.write_png(name+'.png')
     print(name+".png created")
+
 
 def draw_cost(estimations, oob_estimation, acc_estimation,  depth):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_title('Test performance')
-    ax.plot(estimations, oob_estimation, label ="OOB rate") 
-    ax.plot(estimations, acc_estimation, label = "Accuracy")
-    # ax.plot(depth, color='r' ,label='Maximum Depth')
+    ax.plot(estimations, oob_estimation, label="OOB rate")
+    ax.plot(estimations, acc_estimation, label="Accuracy")
     ax.set_xlabel('n estimator')
     ax.set_ylabel("Rate of accuracy or OOB") 
     ax.legend()
     plt.show()
 
 
-def rand_forest_n_Fold(): 
-    entries = os.environ["HOME"] + config.image_location
-    parasitized_entries = os.listdir(entries + '/Parasitized')
-    uninfected_entries = os.listdir(entries + '/Uninfected')
-
+def rand_forest_n_fold():
     image, features, labels = load_image_data() 
-    print('loaded feature shape:',features.shape)
-    df = pd.DataFrame(features) 
+    print('loaded feature shape:', features.shape)
     
-    #needs for drawing trees 
+    # Conversions needed for drawing trees
     feature_list = pd.Series(features[0])
     features = np.array(features)
 
+    # Split data
     train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.5,
                                                                                 random_state=42) 
-    print('train feature shape:',train_features.shape) 
+    print('train feature shape:', train_features.shape)
     print('test feature shape:', test_features.shape) 
 
-    n_best=100
-    acc =0
+    n_best = 100
+    acc = 0
     depth_list = []
     acc_estimation = []
-    oob_estimation= []
-    #parameter sweep 
-    estimations = [10,100, 150, 300, 500 ,1000]
-    for n_estimationin in estimations: 
-        print("##### n estimation",n_estimationin) 
-        rf = RandomForestClassifier(warm_start=True,n_estimators=n_estimationin, criterion ="entropy", oob_score=True)
-        acc_i, depth_tree= train_random_forest(train_features, test_features, train_labels, test_labels,feature_list,rf)
+    oob_estimation = []
+
+    # parameter sweep
+    estimations = [10, 100, 150, 300, 500, 1000]
+    for estimation in estimations:
+        print("##### n estimation", estimation)
+        rf = RandomForestClassifier(warm_start=True, n_estimators=estimation, criterion="entropy", oob_score=True)
+        acc_i, depth_tree = train_random_forest(train_features, test_features, train_labels, test_labels,
+                                                feature_list, rf)
         print("OOB score (generalization accuracy?)", rf.oob_score_)
-        tree = rf.estimators_[0]
         oob_estimation.append(rf.oob_score_)
         acc_estimation.append(acc_i)
         depth_list.append(depth_tree)  
         if acc_i > acc:
-            n_best = n_estimationin 
+            n_best = estimation
             acc = acc_i 
 
-    #best setting 
+    # best setting
     print("best setting: n =", n_best)
     
-    rf = RandomForestClassifier(n_estimators=n_best, random_state=42, criterion ="entropy")
-    acc_i = train_random_forest(train_features, test_features, train_labels, test_labels,feature_list,rf)
+    rf = RandomForestClassifier(n_estimators=n_best, random_state=42, criterion="entropy")
     draw_tree(rf, feature_list, "Tree") 
  
     print("###########Important features")
@@ -109,19 +101,20 @@ def rand_forest_n_Fold():
     [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in zip(range(20), feature_importances)]
     
     sfm = SelectFromModel(rf, threshold=0.04) 
-    sfm.fit(train_features, train_labels) 
-    #reducing number of features in test and train 
+    sfm.fit(train_features, train_labels)
+
+    # reducing number of features in test and train
     train_selected_feature = sfm.transform(train_features)
-    test_selected_feature = sfm.transform(test_features) 
-    print("selected features shapes",train_selected_feature.shape)
+    print("selected features shapes", train_selected_feature.shape)
     print("selected feature::::", train_selected_feature)
 
-    #training on selected features only 
-    rf_important = RandomForestClassifier(warm_start=True,n_estimators=n_best, random_state=42, oob_score=True,criterion ="entropy")
-    acc , depth= train_random_forest(train_selected_feature, test_selected_feature, train_labels, test_labels, feature_list, rf_important) 
-    feature_list= pd.Series(train_selected_feature[0])
-    print("RF traind from selected features OOB", rf_important.oob_score_) 
+    # training on selected features only
+    rf_important = RandomForestClassifier(warm_start=True, n_estimators=n_best, random_state=42, oob_score=True,
+                                          criterion="entropy")
+    feature_list = pd.Series(train_selected_feature[0])
+    print("RF trained from selected features OOB", rf_important.oob_score_)
     draw_tree(rf_important, feature_list, "selectedTree") 
-    draw_cost(estimations, oob_estimation , acc_estimation,  depth_list)
+    draw_cost(estimations, oob_estimation, acc_estimation,  depth_list)
 
-rand_forest_n_Fold()
+
+rand_forest_n_fold()
