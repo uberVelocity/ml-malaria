f1cbdad9a6d307d18d6d5bc7694f49ee15666d9f
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2020-02-01 22:14:44 +0100

    added confusion matrix stuff

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
 from data_wrappers import load_image_data
+from sklearn.metrics import confusion_matrix
 
 
 def ten_folds_knn(k):
@@ -15,10 +16,11 @@ def ten_folds_knn(k):
     kf = KFold(n_splits=10, shuffle=True)  # Set values for K-Fold
     k_folds_iterator = 1
     scores = []
+    conv_matrix = np.array([[0, 0], [0, 0]])
 
     # For each fold selected as test fold
     for train_index, test_index in kf.split(labels):
-        print(f"{k} folds iteration {k_folds_iterator}")
+        print(f"{k}-nn iteration {k_folds_iterator}")
         k_folds_iterator += 1
 
         # Get fold data
@@ -30,10 +32,23 @@ def ten_folds_knn(k):
         knn.fit(train_features, train_labels)
 
         # Testing
+        predictions = knn.predict(test_features)
         score = knn.score(test_features, test_labels)
         scores.append(score)
+        conv_matrix += confusion_matrix(test_labels, predictions)
+
     print("Cross validation done, results:")
     print(f"Mean accuracy: {round(np.mean(scores)*100, 1)}, std: {round(np.std(scores)*100, 1)}")
+    conv_matrix = conv_matrix / 10.0
+    tn = conv_matrix[0][0]
+    fn = conv_matrix[1][0]
+    tp = conv_matrix[1][1]
+    fp = conv_matrix[0][1]
+    specificity = tn/(tn+fn)
+    sensitivity = tp/(tp+fp)
+
+    print(f"True negative: {tn}, \nFalse negative: {fn}, \nTrue positive: {tp}, \nFalse positives: {fp}")
+    print(f"Sensitivity: {sensitivity}, specificity: {specificity}")
 
     return round(np.mean(scores)*100, 1), round(np.std(scores)*100, 1)
 
@@ -62,4 +77,4 @@ def find_optimal_k():
 
 
 if __name__ == '__main__':
-    find_optimal_k()
+    ten_folds_knn(7)
