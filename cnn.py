13527faea50b6945aff0deef4aea9fec79c30315
ubervelocity <mihaitu.popescu@gmail.com>
2020-02-01 17:15:34 +0100

    refactored cnn.py

--- cnn.py
+++ cnn.py
@@ -1,10 +1,11 @@
 import numpy as np
 import config
+import data_wrappers
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import datasets, layers, models
-from data_processing_wrappers import load_images
-
 
 def create_model():
     """
@@ -15,9 +16,9 @@ def create_model():
     # Convolutional layers
     model.add(layers.Conv2D(32, (3, 3), activation='relu',
                             input_shape=(config.scaled_size[1], config.scaled_size[0], 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.MaxPooling2D(2, 2))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
     # Flatten the tensors and create Dense layers for classification
@@ -29,26 +30,41 @@ def create_model():
 
     return model
 
+def plot_val_accuracies(val_accuracies):
+    plt.plot(val_accuracies, '-o')
+    plt.ylabel('Validation accuracy')
+    plt.xlabel('Epoch')
+    plt.title('CNN Validation accuracy across 10 epochs')
+    plt.show()
 
 if __name__ == '__main__':
-    print("Loading images...")
-    images, labels = load_images()
+    # Load images
+    print('Loading images...')
+    features, labels = data_wrappers.load_images()
 
-    n_folds = 10
+    # Check images dimensions
+    print(features.shape)
+    print(labels.shape)
+
+    # Create CNN model
+    print('\n Creating model...')
+    model = create_model()
+    model.summary()
+
+    # Compile model
+    print('Compiling...')
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    print('Compiling done! Commencing training...')
+    
+    # Fit the model
     BATCH_SIZE = 128
-    val_accuracy = [None] * 10
-    for i in range(n_folds):
-        print('Training on fold ', i + 1)
-        print("\n Creating model...")
-        model = create_model()
-        model.summary()
-        
-        print("Compiling...")
-        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-        print("Compiling done! Commencing training...")
-        
-        x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=1/n_folds, random_state=i)
-        val_accuracy[i] = model.fit(x_train, y_train, epochs=10, batch_size=BATCH_SIZE,
-                                    validation_data=(x_test, y_test))
-    print('validation accuracies: ', val_accuracy)
-    print('mean accuracy: ', np.mean(val_accuracy))
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=1/3, random_state=42)
+    val_accuracy = model.fit(x_train, y_train, epochs=3,
+                                validation_data=(x_test, y_test))
+
+    # Print validation accuracy scores
+    print('val_accuracy: ', val_accuracy.history['val_accuracy'])
+
+    # Plot validation accuracy scores
+    plot_val_accuracies(val_accuracy.history['val_accuracy'])
+
