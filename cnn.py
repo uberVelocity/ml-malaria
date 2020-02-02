710615e66604ec92b94e755a54ad1ec5e03d4690
ubervelocity <mihaitu.popescu@gmail.com>
2020-02-02 21:34:43 +0100

    confusion matrix and comments regarding tensorflow version

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ import config
 import data_wrappers
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import sklearn.metrics as skmet
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import datasets, layers, models
@@ -51,6 +52,7 @@ if __name__ == '__main__':
         labels[labels == 'parasitized'] = 0
     if 'uninfected' in labels:
         labels[labels == 'uninfected'] = 1
+
     # Convert strings to ints
     labels = list(map(int, labels))
     labels = np.array(labels)
@@ -60,9 +62,11 @@ if __name__ == '__main__':
     print('\n Creating model...')
     model = create_model()
     model.summary()
+    print(tf.keras.metrics)
 
     # Compile model
     print('Compiling...')
+
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     print('Compiling done! Commencing training...')
     
@@ -71,10 +75,39 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=1/3, random_state=42)
     val_accuracy = model.fit(x_train, y_train, epochs=10,
                                 validation_data=(x_test, y_test))
+    
+    # Sample the data once more for good measure
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=1/3, random_state=10)
+    
+    # Predict data using trained classifier
+    y_pred = model.predict(x_test)
 
-    # Print validation accuracy scores
+    # Convert predictons to binary class output
+    y_pred_binary = [None] * len(y_pred)
+    for i in range(0, len(y_pred)):
+        print(y_pred[i][0], " : ", y_pred[i][1])
+        if y_pred[i][0] > y_pred[i][1]:
+            y_pred_binary[i] = 0
+        else:
+            y_pred_binary[i] = 1
+    
+    # Extract tn, fp, fn, tp
+    tn, fp, fn, tp = skmet.confusion_matrix(y_test, y_pred_binary).ravel();
+    print(tp, fp, tn, fn)
+
+    # Compute sensitivity and specificity
+    sensitivity = tp / (tp + fp)
+    specificity = tn / (tn + fn)
+    print("sensitivity: ", sensitivity)
+    print("specificity: ", specificity)
+
+    # In order to create the plot using this function, you should have Tensorflow version 2.1.0
+    # Check tensorflow version by doing `import tensorflow as tf; print(tf.__version__);
+    # Print validation accuracy scores - commented by default.
     # print('val_accuracy: ', val_accuracy.history['val_accuracy'])
 
-    # Plot validation accuracy scores
+    # Plot validation accuracy scores - Commented by default.
     # plot_val_accuracies(val_accuracy.history['val_accuracy'])
+
+    
     
